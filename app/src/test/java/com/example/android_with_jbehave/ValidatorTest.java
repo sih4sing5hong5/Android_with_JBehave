16f321f4ae7a6ba47aca5bb577767697d4f1cafe
薛丞宏 <ihcaoe@gmail.com>
2015-08-13 09:32:22 +0800

    加junit佮assertj試驗範例

--- app/src/test/java/com/example/android_with_jbehave/ValidatorTest.java
+++ app/src/test/java/com/example/android_with_jbehave/ValidatorTest.java
@@ -1,32 +1,22 @@
 package com.example.android_with_jbehave;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import java.util.regex.Pattern;
-
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
-import static org.mockito.Mockito.*;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import android.content.SharedPreferences;
 
 
 public class ValidatorTest {
 
     @Test
-    public void success() {
+    public void junitTestStyle() {
+
         assertTrue(true);
     }
 
     @Test
-    public void fail() {
-        assertTrue(false);
+    public void assertjTestStyle() {
+        Assertions.assertThat(333).isEqualTo(333);
     }
+
 }
